2673052c442cb55d29b7c4b1f1a5337c62382d6e
Parth Marfatia <parthmarfatia153@gmail.com>
2022-07-14 19:36:57 +0530

    merging all changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hadsf");
+// Api call
+async function start() {
+  try {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log("problem!");
+  }
+}
+
+// Button click function
+var btnClick = async () => {
+  const data = await start();
+  document.getElementById("quoteText").innerHTML = `"${data.content}"`;
+  document.getElementById("quoteAuthor").innerHTML = `${data.author}`;
+};
